377d365d6370b40f1eea676a46d8a5080ebce709
amiraBenamer20 <amira.rayane.benamer@gmail.com>
2024-01-04 18:45:28 +0100

    new changes

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -17,8 +17,24 @@ resource "aws_security_group" "devops2_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  //jenkins on 8080
+  ingress {
+    description      = "custom tcp"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "allow_ssh"
+    Name = "allow_rules"
   }
 }
 
